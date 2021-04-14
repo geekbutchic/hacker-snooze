41ef6f1add3948abe76d1cbcf96876b9b4a80c16
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-04-14 10:23:22 -0400

    adds functionality

---
+++ main.js
@@ -0,0 +1,10 @@
+const body = document.querySelector('body');
+const parent = document.createElement('div');
+parent.className = 'parent';
+
+const child = document.createElement('div');
+child.className = 'child';
+
+parent.appendChild(child);
+body.appendChild(parent);
+
